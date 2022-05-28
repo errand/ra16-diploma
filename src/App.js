0ea7aa6d9c770dbadbb76b36b83ec856b13ad853
Aleksandr Shatskikh <work@errand.ru>
2022-05-28 15:51:33 +0300

    feat: added static pages and banner

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from "./pages/About";
 import Catalog from "./pages/Catalog";
 import Contacts from "./pages/Contacts";
 import Footer from "./components/Foter";
+import NotFound from "./pages/NotFound";
+import Banner from "./components/Banner";
 
 import './App.css';
 
@@ -12,13 +14,20 @@ function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="catalog" element={<Catalog />} />
-        <Route path="contacts" element={<Contacts />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <main className="container">
+        <div className="row">
+          <div className="col">
+            <Banner />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="catalog" element={<Catalog />} />
+              <Route path="contacts" element={<Contacts />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,24 @@
 export default function Home() {
   return (
-    <main className="container">
-      <div className="row">
-        <div className="col">
-          <div className="banner">
-            <img src="./img/banner.jpg" className="img-fluid" alt="К весне готовы!" />
-              <h2 className="banner-header">К весне готовы!</h2>
-          </div>
-          <section className="top-sales">
-            <h2 className="text-center">Хиты продаж!</h2>
-            <div className="preloader">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </section>
-          <section className="catalog">
-            <h2 className="text-center">Каталог</h2>
-            <div className="preloader">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </section>
+    <>
+      <section className="top-sales">
+        <h2 className="text-center">Хиты продаж!</h2>
+        <div className="preloader">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-      </div>
-    </main>
+      </section>
+      <section className="catalog">
+        <h2 className="text-center">Каталог</h2>
+        <div className="preloader">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </section>
+    </>
   )
 }
